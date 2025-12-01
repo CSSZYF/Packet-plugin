bc131031ba34b15bffce84329b7b2e317a7ae0fd
CSSZYF <63802741+CSSZYF@users.noreply.github.com>
2025-12-02 03:23:37 +0800

    Refactor packet variable to pb in PacketHelper.js

--- model/PacketHelper.js
+++ model/PacketHelper.js
@@ -56,7 +56,7 @@ export const Elem = async (
   content
 ) => {
   try {
-    const packet = {
+    const pb = {
       "1": {
         [e.isGroup ? "2" : "1"]: {
           "1": e.isGroup ? e.group_id : e.user_id
@@ -76,7 +76,7 @@ export const Elem = async (
       "5": RandomUInt()
     }
 
-    return Send(e, 'MessageSvc.PbSendMsg', packet)
+    return Send(e, 'MessageSvc.PbSendMsg', pb)
   } catch (error) {
     logger.error(`sendMessage failed: ${error.message}`, error)
   }
@@ -127,7 +127,7 @@ export const sendLong = async (
   const compressedData = await gzip(pb.encode(data))
   const target = e.isGroup ? BigInt(e.group_id) : e.user_id
 
-  const packet = {
+  const pb = {
     "2": {
       "1": e.isGroup ? 3 : 1,
       "2": {
@@ -144,7 +144,7 @@ export const sendLong = async (
     }
   }
 
-  const resp = await Send(e, 'trpc.group.long_msg_interface.MsgService.SsoSendLongMsg', packet)
+  const resp = await Send(e, 'trpc.group.long_msg_interface.MsgService.SsoSendLongMsg', pb)
   return resp?.["2"]?.["3"]
 }
 
@@ -152,7 +152,7 @@ export const recvLong = async (
   e,
   resid
 ) => {
-  const packet = {
+  const pb = {
     "1": {
       "2": resid,
       "3": true
@@ -165,7 +165,7 @@ export const recvLong = async (
     }
   }
 
-  const resp = await Send(e, 'trpc.group.long_msg_interface.MsgService.SsoRecvLongMsg', packet)
+  const resp = await Send(e, 'trpc.group.long_msg_interface.MsgService.SsoRecvLongMsg', pb)
   return pb.decode(await gunzip(resp?.["1"]?.["4"]))
 }
 
@@ -179,7 +179,7 @@ export const getMsg = async (
   }))?.real_seq)
   if (!seq) throw new Error("获取seq失败，请尝试更新napcat")
 
-  const packet = {
+  const pb = {
     "1": {
       "1": e.group_id,
       "2": seq,
@@ -188,7 +188,7 @@ export const getMsg = async (
     "2": true
   }
 
-  return Send(e, 'trpc.msg.register_proxy.RegisterProxy.SsoGetGroupMsg', packet)
+  return Send(e, 'trpc.msg.register_proxy.RegisterProxy.SsoGetGroupMsg', pb)
 }
 
 // 仅用于方便用户手动输入pb时使用，一般不需要使用
